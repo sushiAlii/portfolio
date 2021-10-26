30f380eb2b34bfa48ea05139e8cdd918b054b70d
sushiAlii <aliasgar.laut@gmail.com>
2021-10-27 00:11:29 +0800

    Creating a short essay about me :3

--- components/AboutContent.tsx
+++ components/AboutContent.tsx
@@ -1,15 +1,20 @@
 import { Flex, Center, Text, Container } from "@chakra-ui/react";
 
 export const AboutContent = () => {
+  const aboutMe: string =
+    "Hi! I'm Ali! a freelance software engineer based in the Philippines." +
+    "My passion in programming started in college during data structure classes. Back then I didn't really intended to strive being a software engineer," +
+    'and here I am writing an "about me" content for my portfolio. ';
+
   return (
-    <Center mt={10}>
+    <Center h="100%">
       <div>
         <Text fontSize="4xl" align="center">
-          About Me
+          About Me 🍣
         </Text>
 
-        <Container m={10} p={10} bg="blue.300">
-          <Text>Hi! I`m Ali, a software engineer based in Philippines.</Text>
+        <Container maxW="container.md" m={10} p={10}>
+          <Text align="center">{aboutMe}</Text>
         </Container>
       </div>
     </Center>
