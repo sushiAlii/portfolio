a5db195ff3c7d3081bbaccab831bf92a0b945f49
sushiAlii <aliasgar.laut@gmail.com>
2021-10-28 01:00:59 +0800

    Finalizing Contact Me Content

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -4,27 +4,29 @@ export const ContactMe = () => {
   const email: string = "aliasgar.laut@gmail.com";
   return (
     <>
-      <Center>
-        <Text fontSize="4xl" color="gray.400">
-          Don`t be Shy
-        </Text>
-      </Center>
-      <Center my={4}>
-        <Box maxW={400}>
-          <Text color="gray.500" align="center">
-            I`m always eager to sharpen my skills, and is open to any
-            <Text as="del" color="gray.400">
-              {" "}
-              (I mean..not really any)
-            </Text>{" "}
-            opportunities! My inbox is always open and I`ll always send you a
-            reply! Email me at{" "}
-            <Text as="kbd" color="gray.400">
-              <Link href={"mailto:" + email}>{email}</Link>
-            </Text>
+      <Box py={10}>
+        <Center>
+          <Text fontSize="4xl" color="gray.400">
+            Don`t be Shy
           </Text>
-        </Box>
-      </Center>
+        </Center>
+        <Center my={4}>
+          <Box maxW={400}>
+            <Text color="gray.500" align="center">
+              I`m always eager to sharpen my skills, and is open to any
+              <Text as="del" color="gray.400">
+                {" "}
+                (I mean..not really any)
+              </Text>{" "}
+              opportunities! My inbox is always open and I`ll always send you a
+              reply! Email me at{" "}
+              <Text as="kbd" color="gray.400">
+                <Link href={"mailto:" + email}>{email}</Link>
+              </Text>
+            </Text>
+          </Box>
+        </Center>
+      </Box>
     </>
   );
 };
