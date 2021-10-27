0e13528dcfe421df3a86f4abc7e632e9ca820cab
sushiAlii <aliasgar.laut@gmail.com>
2021-10-27 23:16:58 +0800

    misc

--- components/ProjectContent.tsx
+++ components/ProjectContent.tsx
@@ -18,6 +18,7 @@ interface Props {
     align: string;
     description: string;
     descriptionAlign: string;
+    tools: string;
   };
 }
 
@@ -25,47 +26,72 @@ export const ProjectContent: React.FC<Props> = ({ index, project }) => {
   if (index % 2 === 0) {
     return (
       <WrapItem key={index}>
-        <Box mt={5} outlineColor="whiteAlpha.100" width={1000}>
+        <Box mt={5} width={1000}>
           <Flex justify={project.align}>
             <Img
               m={10}
-              borderRadius="lg"
+              borderRadius="md"
               boxShadow="dark-lg"
               src={project.src}
               alt={project.name}
-              maxW={450}
+              maxW={550}
             />
 
-            <Flex justify={project.descriptionAlign}>
-              <Text fontSize="2xl" m={10}>
-                {project.name}
-              </Text>
+            <Flex justify={project.descriptionAlign} w="100%" align="center">
+              <Wrap m={10}>
+                <Text align="end">
+                  <Box pb={4}>
+                    <Text fontSize="3xl" color="gray.400">
+                      {project.name}
+                    </Text>
+                    <Text fontSize="md" color="gray.500">
+                      {project.description}
+                    </Text>
+                  </Box>
+
+                  <Text as="kbd" fontSize="sm" color="gray.400">
+                    {project.tools}
+                  </Text>
+                </Text>
+              </Wrap>
             </Flex>
-          </Flex>
-        </Box>
-      </WrapItem>
-    );
-  } else {
-    return (
-      <WrapItem key={index}>
-        <Box mt={5} outlineColor="whiteAlpha.100" width={1000}>
-          <Flex justify={project.align}>
-            <Flex justify={project.descriptionAlign}>
-              <Text fontSize="2xl" m={10}>
-                {project.name}
-              </Text>
-            </Flex>
-            <Img
-              m={10}
-              borderRadius="lg"
-              boxShadow="dark-lg"
-              src={project.src}
-              alt={project.name}
-              maxW={450}
-            />
           </Flex>
         </Box>
       </WrapItem>
     );
   }
+  return (
+    <WrapItem key={index}>
+      <Box mt={5} outlineColor="whiteAlpha.100" width={1000}>
+        <Flex justify={project.align}>
+          <Flex justify={project.descriptionAlign} w="100%" align="center">
+            <Wrap m={10}>
+              <Text align="start">
+                <Box pb={4}>
+                  <Text fontSize="3xl" color="gray.400">
+                    {project.name}
+                  </Text>
+                  <Text fontSize="md" color="gray.500">
+                    {project.description}
+                  </Text>
+                </Box>
+
+                <Text as="kbd" fontSize="sm" color="gray.400">
+                  {project.tools}
+                </Text>
+              </Text>
+            </Wrap>
+          </Flex>
+          <Img
+            m={10}
+            borderRadius="md"
+            boxShadow="dark-lg"
+            src={project.src}
+            alt={project.name}
+            maxW={550}
+          />
+        </Flex>
+      </Box>
+    </WrapItem>
+  );
 };
