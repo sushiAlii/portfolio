5ae71a165dc59e021530a42c47f6d6231ee21968
sushiAlii <aliasgar.laut@gmail.com>
2021-10-30 22:54:28 +0800

    Applying new background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />;
+      <Box backgroundImage="url(/assets/background.svg)">
+        <Box>
+          <Component {...pageProps} />;
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
